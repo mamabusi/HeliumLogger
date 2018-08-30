38e8c917f253a08874a293eb25bdbbd37e489274
Mamatha Busi <mamabusi@in.ibm.com>
2018-08-30 11:10:12 +0530

    Modified Logger package url

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMinor(from: "1.7.0"))
+        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
